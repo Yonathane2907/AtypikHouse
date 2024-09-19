0ad57ef7c422d97cce9d8f6407dee6f9b4cbf5b7
Yonathane2907 <nathane5760@gmail.com>
2024-09-19 15:50:00 +0200

    test

--- atypik_house/lib/screens/AboutScreen.dart
+++ atypik_house/lib/screens/AboutScreen.dart
@@ -26,42 +26,22 @@ class AboutPage extends StatelessWidget {
                     ),
                     SizedBox(height: 16.0),
                     Text(
-                      'Nom de l\'Application',
+                      'Qui sommes-nous ?',
                       style: Theme.of(context).textTheme.titleMedium, // Utilisation de titleMedium ou headline6
                     ),
                     SizedBox(height: 8.0),
                     Text(
-                      'Cette application est conçue pour fournir des fonctionnalités X, Y, et Z. Elle vise à aider les utilisateurs à accomplir A, B, et C de manière efficace et intuitive.',
+                      'AtypikHouse est le fruit de la passion commune de trois associées, animées par un amour profond pour les voyages, l\'habitat alternatif et un mode de vie en parfaite harmonie avec la nature. \n Ensemble, nous mettons tout en œuvre pour vous offrir une sélection exclusive de logements atypiques, pour que chaque séjour devienne une aventure inoubliable et une escapade hors du commun.',
                       style: Theme.of(context).textTheme.bodyLarge, // Utilisation de bodyLarge ou bodyText1
                     ),
                     SizedBox(height: 16.0),
                     Text(
-                      'Développeur',
+                      'Avec Atypikhouse',
                       style: Theme.of(context).textTheme.titleMedium, // Utilisation de titleMedium ou headline6
                     ),
                     SizedBox(height: 8.0),
                     Text(
-                      'Développé par [Votre Nom]. Vous pouvez nous contacter à l\'adresse suivante : [votre.email@example.com]. Nous apprécions vos retours et suggestions pour améliorer l\'application.',
-                      style: Theme.of(context).textTheme.bodyLarge, // Utilisation de bodyLarge ou bodyText1
-                    ),
-                    SizedBox(height: 16.0),
-                    Text(
-                      'Version',
-                      style: Theme.of(context).textTheme.titleMedium, // Utilisation de titleMedium ou headline6
-                    ),
-                    SizedBox(height: 8.0),
-                    Text(
-                      'Version 1.0.0',
-                      style: Theme.of(context).textTheme.bodyLarge, // Utilisation de bodyLarge ou bodyText1
-                    ),
-                    SizedBox(height: 16.0),
-                    Text(
-                      'Remerciements',
-                      style: Theme.of(context).textTheme.titleMedium, // Utilisation de titleMedium ou headline6
-                    ),
-                    SizedBox(height: 8.0),
-                    Text(
-                      'Merci à tous ceux qui ont contribué au développement de cette application, y compris les développeurs de bibliothèques open-source et la communauté Flutter.',
+                      'Nous vous proposons une sélection variée de logements atypiques pour des séjours uniques et mémorables. Du charme d\'une cabane dans les arbres à \'élégance d\'une bulle insolite, en passant par le confort d\'une maison flottante, nos hébergements vous offrent une expérience hors du commun.\n Chaque logement a été soigneusement choisi pour son caractère unique, son emplacement en harmonie avec la nature et les souvenirs inoubliables qu\'il vous procurera. Que vous recherchiez une escapade romantique, une aventure en famille ou une expérience insolite entre amis, Atypik House vous ouvre les portes d\'un monde de possibilités.\n Nos logements sont également conçus pour respecter l\'environnement, avec une approche écoresponsable et durable. Nous croyons en un tourisme respectueux de la nature et des communautés locales, et nous nous engageons à préserver la beauté de nos destinations.\n Préparez-vous à vivre des moments inoubliables et à créer des souvenirs impérissables dans nos logements atypiques. Réservez dès maintenant votre prochaine aventure avec Atypik House !',
                       style: Theme.of(context).textTheme.bodyLarge, // Utilisation de bodyLarge ou bodyText1
                     ),
                   ],
